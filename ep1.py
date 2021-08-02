14b1a90516fedc79234b23d5573af2a960558058
tententgc <nyman3527@gmail.com>
2021-08-02 23:03:52 +0700

    first

---
+++ ep1.py
@@ -0,0 +1,18 @@
+a = 5
+b="Hello"
+c =5.8
+d= True
+e = False
+a = 10
+print('tenten' + b)
+
+# x,y,z = 10,20,30
+
+# x=y=z=30
+# print(x,y,z)
+# # print(type(a))
+# # print(type(b))
+# # print(type(c))
+# # print(type(d))
+# # print(type(e))
+# int  str  float booleans
